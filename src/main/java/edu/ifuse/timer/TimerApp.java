47c6cc0d9e2e3b5466be1efe4aa9a2db0d99b109
Vova Kapustynskyi <kapust24032003@gmail.com>
2022-06-19 14:24:45 +0300

    change logging format

--- src/main/java/edu/ifuse/timer/TimerApp.java
+++ src/main/java/edu/ifuse/timer/TimerApp.java
@@ -81,6 +81,7 @@ public class TimerApp {
 
     private static void configureLogger() {
         try {
+            System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tF %1$tT] [%4$-7s] %5$s %n");
             LOGGER = Logger.getLogger("myLogger");
             FileHandler fileHandler = new FileHandler("log.txt");
             LOGGER.addHandler(fileHandler);
